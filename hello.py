a8731dd5329cc52b20c2844e21de878e2151ad32
jhy1000 <jhy1000@gmail.com>
2018-06-03 22:04:57 +0800

    modify hello.py

--- hello.py
+++ hello.py
@@ -5,6 +5,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail,Message
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -15,9 +16,25 @@ app.config['SECRET_KEY'] = 'hard to guess string'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
+app.config['MAIL_SERVER'] = 'smtp.126.com'
+app.config['MAIL_PORT'] = 25
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME') or 'jianghengyong@126.com'
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD') or '.jhy1234'
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
+app.config['FLASKY_MAIL_SENDER'] = 'jianghengyong@126.com'
+app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN') or 'jianghengyong@126.com'
+
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
+mail = Mail(app)
 
+def send_mail(to,subject,template,**kwargs):
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
+            sender=app.config['FLASKY_MAIL_SENDER'],recipients=[to])
+    msg.body = render_template(template + '.txt',**kwargs)
+    msg.html = render_template(template + '.html',**kwargs)
+    mail.send(msg)
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -52,6 +69,8 @@ def index():
             user = User(username=form.name.data)
             db.session.add(user)
             session['known'] = False
+            if app.config['FLASKY_ADMIN']:
+                send_mail(app.config['FLASKY_ADMIN'],'New User','mail/new_user',user=user)
         else:
             session['known'] = True
         session['name'] = form.name.data
@@ -59,16 +78,6 @@ def index():
         return redirect(url_for('index'))
     return render_template('index.html',form=form, name=session.get('name'),known=session.get('known',False))
 
-#@app.route('/',methods=['GET','POST'])
-#def index():
-#    form = NameForm()
-#    if form.validate_on_submit():
-#        old_name = session.get('name')
-#        if old_name is not None and old_name != form.name.data:
-#            flash('Looks like you have changed you name!')
-#        session['name'] = form.name.data
-#        return redirect(url_for('index'))
-#    return render_template('index.html',form=form, name=session.get('name'))
 
 @app.errorhandler(404)
 def page_not_found(e):
