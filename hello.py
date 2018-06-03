bb0987a3ced165da6caec230aec70392710e064d
jhy1000 <jhy1000@gmail.com>
2018-06-03 21:31:26 +0800

    modify hello.py

--- hello.py
+++ hello.py
@@ -1,13 +1,74 @@
-from flask import Flask,request,redirect,abort,render_template
+from flask import Flask,request,redirect,abort,render_template,session,url_for
 from flask import make_response
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 app = Flask(__name__)
-bootstrap = Bootstrap(app)
+app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
-@app.route('/')
+bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64),unique=True)
+    user = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64),unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+class NameForm(FlaskForm):
+    name = StringField("What is you name?",validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+@app.route('/',methods=['GET','POST'])
 def index():
-    return render_template('user.html',name='jhy')
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('index'))
+    return render_template('index.html',form=form, name=session.get('name'),known=session.get('known',False))
+
+#@app.route('/',methods=['GET','POST'])
+#def index():
+#    form = NameForm()
+#    if form.validate_on_submit():
+#        old_name = session.get('name')
+#        if old_name is not None and old_name != form.name.data:
+#            flash('Looks like you have changed you name!')
+#        session['name'] = form.name.data
+#        return redirect(url_for('index'))
+#    return render_template('index.html',form=form, name=session.get('name'))
 
 @app.errorhandler(404)
 def page_not_found(e):
